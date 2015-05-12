022e87d972e31fde5233b2cd152aa7b4eabe3f57
warddalston <ward.dalston@gmail.com>
2015-05-12 16:55:26 -0500

    Functions for cov balance

--- MultiSynthCovBalance.R
+++ MultiSynthCovBalance.R
@@ -1,10 +1,11 @@
 MultiSynthCovBalance <- function(MultiSynthPrep_obj, MultiSynth_obj, begin_time, end_time){
   
-  #Calculate the pre treatment error ratios here
+  #extract the covariate balance here 
   out <- sapply(MultiSynth_obj, function(synth){ return(synth$loss.w)})
   names(out) <- sapply(MultiSynthPrep_obj, function(dataprep){ return(as.character(dataprep$names.and.numbers$unit.names[ dataprep$names.and.numbers$unit.numbers == dataprep$tag$treatment.identifier]))})
   
   #so that I can print them in a pretty way
-  class(out) = "MultiSynthErrorRatio"
+  class(out) <- "MultiSynthCovBalance"
+  comment(out) <- class(MultiSynthPrep_obj) 
   return(out)
 }
---
+++ printMultiSynthCovBalance.R
@@ -0,0 +1,12 @@
+print.MultiSynthCovBalance <- function(obj, sorted = FALSE, digits = 4){
+  
+  #sort them from lowest to highest
+  if(sorted){ to_print <- sort(obj) } else{ to_print <- obj }
+  
+  cat(" ************ Multi Synth ", comment(obj)[1], " Covariate Loss ********** \n", sep="")
+  sapply(to_print, function(value){
+    cat( names(to_print)[to_print == value], ": ", round(value, digits), "\n", sep = "")
+  })
+  
+  return()
+}
