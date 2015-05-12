bf8d3703446bfb2feefa5934f667a88e9ec3cd73
warddalston <ward.dalston@gmail.com>
2015-05-12 14:06:07 -0500

    Multi Synth
    Add ability to take additional arguments to Multi Synth

--- MultiSynth.R
+++ MultiSynth.R
@@ -1,4 +1,4 @@
-
-MultiSynth <- function(MultiSynthPrep_obj, parallel = FALSE){
-  llply(.data = MultiSynthPrep_obj, .fun = synth, .parallel = parallel)
+MultiSynth <- function(MultiSynthPrep_obj, parallel = FALSE,...){
+  llply(.data = MultiSynthPrep_obj, .fun = synth, ... , .parallel = parallel)
 }
+
