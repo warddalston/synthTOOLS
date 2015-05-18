00dc189e269b50166618b798704dd25d4a78f5de
warddalston <ward.dalston@gmail.com>
2015-05-18 16:58:00 -0500

    Lots of changes.
    Moved things to helpful places.

--- MultiSynthPathPlot.R
+++ MultiSynthPathPlot.R
@@ -1,15 +1,19 @@
 setMethod(f = "path.plot",
           signature = "MultiSynth",
-          def = function(synth.res = input, dataprep.res = NA, tr.intake = NA, Ylab = c("Y Axis"), Xlab = c("Time"), Ylim = NA, Legend = c("Treated", "Synthetic"), Legend.position = c("topright"), Main = NA, Z.plot = FALSE){
+          def = function(synth.res = input, dataprep.res = NA, tr.intake = NA, Ylab = c("Y Axis"), Xlab = c("Time"), Ylim = NA, Legend = c("Treated", "Synthetic", "Alternative Synthetics"), Legend.position = c("topleft"), Main = NA, Z.plot = FALSE){
             
             #plot the basic thing
-            path.plot(synth.res@fits[[1]], synth.res@input, synth.res@treatment_time, Ylab = Ylab, Xlab= Xlab, Ylim = Ylim, Legend = Legend, Legend.position = Legend.position, Main = Main, Z.plot = Z.plot)
+            path.plot(synth.res@fits[[1]], synth.res@input, synth.res@treatment_time, Ylab = Ylab, Xlab= Xlab, Ylim = Ylim, Legend = NA, Legend.position = Legend.position, Main = Main, Z.plot = Z.plot)
             
             #prep the additional lines
             plot_prep <- MultiSynthPathPlotPrep(synth.res@preps[2:length(synth.res@preps)], synth.res@fits[2:length(synth.res@fits)] )
             
             #plot new lines
-            MultiSynthPlotLines(plot_prep)  
+            MultiSynthPlotLines(plot_prep) 
+            
+            if (sum(is.na(Legend)) == 0) {  
+            legend(Legend.position, legend = Legend, lty = c(1,2,1), col = c("black", "black", "grey"), lwd = c(2, 2, 2), cex = 6/7)
+            }
             
             return(invisible())
           } #end function
