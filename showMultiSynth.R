430ad5bc71d9c96babd4b4f1539d730e8f1c43b4
warddalston <ward.dalston@gmail.com>
2015-05-26 12:02:22 -0500

    Show help file

--- showMultiSynth.R
+++ showMultiSynth.R
@@ -1,3 +1,28 @@
+#' Show objects of class "MultiSynth"
+#' 
+#' Display the basic information about an object of class "MultiSynth".  This prints to the console the type of MultiSynth analysis, information about the treated unit, the donor pool, and covariate set, and the time periods used in fitting the analysis.  The function is invoked by automatic printing (when an object of class "MultiSynth" is sent to the console, or not assigned after a call to \code{\link{fitMultiSynth}}).
+#' 
+#' @usage show(object)
+#' 
+#' @param object An object of class MultiSynth.  Can be a placebo analysis or either type of leave-one-out analysis.  
+#' 
+#' @details There are methods of show defined for each of the three subclasses of MultiSynth.  The only differences between the show methods for these classes is the title of the output printed to the console.  
+#' 
+#' If the user does not provide character unit names in the \code{\link{dataprep}} output used in the call to \code{\link{fitMultiSynth}} then unit numbers are used in the place of unit names for this method.  
+#' 
+#' @return NULL
+#' 
+#' @author Dalston G. Ward: \email{ward.dalston@@wustl.edu}
+#' @references \itemize{
+#' \item Abadie, A., Diamond, A., Hainmueller, J. (2010). Synthetic Control Methods for Comparative Case Studies: Estimating the Effect of California's Tobacco Control Program. \emph{Journal of the American Statistical Association} 105 (490) 493-505.
+#' \item Abadie, A., Diamond, A., Hainmueller, J. (2015). Comparative Politics and the Synthetic Control Method.  \emph{American Journal of Political Science} 59 (2) 495-510
+#' }
+#' 
+#' @seealso \code{\link{fitMultiSynth}}
+#' @seealso \code{\link{MultiSynth}}
+#' 
+#' @rdname show,MultiSynth-method
+#' @export
 setMethod(f = "show",
           signature = "PlaceboMS",
           def = function(object){
@@ -24,6 +49,8 @@ setMethod(f = "show",
           } #end function
           ) #end setMethod
 
+#' @rdname show,MultiSynth-method
+#' @export
 setMethod(f = "show",
           signature = "LOOunitsMS",
           def = function(object){
@@ -50,6 +77,8 @@ setMethod(f = "show",
           } #end function
 ) #end setMethod
 
+#' @rdname show,MultiSynth-method
+#' @export
 setMethod(f = "show",
           signature = "LOOcovariatesMS",
           def = function(object){
@@ -68,17 +97,6 @@ setMethod(f = "show",
                 cat(cov, "\n")
               } else( cat(cov, " "))
             })  
-#             if(!is.null(object@input$tag$special.predictors)){
-#               cat("\n Special Covariates: ")
-#               sapply(object@input$tag$special.predictors, function(spec){
-#                 
-#                 if(length(spec[[2]]) != 1){
-#                   specTime <- paste(spec[[2]][1], ":", spec[[2]][length(spec[[2]])], sep = "")
-#                 } else(specTime <- spec[[2]])
-#                 
-#                 cat(spec[[1]], ", ", specTime, ", ", spec[[3]], "\n", sep = "")
-#               })
-#             }
             cat(" \n****** ****** \n")
             cat("Optimization Period:",min(object@input$tag$time.optimize.ssr), "to", max(object@input$tag$time.optimize.ssr), "\n" )
             cat( "Covariates Measurement Period:",min(object@input$tag$time.predictors.prior), "to", max(object@input$tag$time.predictors.prior), "\n" )
